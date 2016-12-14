bc0e8f542ca11172fa6741ab7adacb2ee0d88ce6
¯\_(ツ)_/¯ <8mpomykala@stpatrick.org>
2016-12-14 11:24:28 -0600

    Add files via upload
    started on item  cubs snapback  jordan retro 5 laser 30th anniversery and gold chain

---
+++ itempom.js
@@ -0,0 +1,11 @@
+class Item{
+  //Fill in the item class below this comment.
+constructor(n,p,s)
+this.name=n
+this.price=p;
+this.shipping=s;
+}
+this.shoes
+this.jewelry
+this.hat
+//Create your three test items below this comment.
