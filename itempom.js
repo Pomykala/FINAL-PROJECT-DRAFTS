4c3cdc4edbcbdcd1921d60bd07059411a9afc9a3
¯\_(ツ)_/¯ <8mpomykala@stpatrick.org>
2016-12-15 10:51:11 -0600

    Add files via upload
    item is complete WOOOOOHOOOO
    
    password is in need of repairs.  specifically in private key and fix brackets  missing of an adition "if" statement

--- itempom.js
+++ itempom.js
@@ -1,11 +1,13 @@
 class Item{
   //Fill in the item class below this comment.
-constructor(n,p,s)
-this.name=n
+constructor(n,p,s){
+this.name=n;
 this.price=p;
 this.shipping=s;
 }
-this.shoes
-this.jewelry
-this.hat
+}
+
 //Create your three test items below this comment.
+let jacket  = new item ("Jordan Windbreaker Military Blue",33,5)
+let shoes = new item ("Jordan Retro 5 Laser 30th Anniversary",44,6)
+let pants = new item ("Jordan Sweatpants Military Blue",55,7)
---
+++ passwordpom.js
@@ -0,0 +1,43 @@
+class Password{
+  //The first part of the class.
+  constructor(p, pk){
+    this.publickey = p;
+    this.privatekey = pk;
+  }
+
+
+  //Instance functions below this comment.
+  validPublicKey(){
+    if(this.publickey.length >= 8 && this.publickey.length <= 25){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  validPrivateKey(){
+    if(this.privatekey[4] == "-" && this.privatekey[9] == "-"){
+      if intenger=<100
+
+    }
+  }
+
+  //Static function below this comment.
+  static makePrivateKey(){
+    let key = "";
+    let limit = 14;
+    for(let d = 0, d < limit, d ++){
+      if(i == 4 || i == 9){
+        key = key + "-";
+      }
+      else{
+        let number = Math.random();
+        number = Math.ceil(number * 10);
+        number = String(number);
+        key = key + number;
+      }
+    }
+    return key;
+  }
+
+}
